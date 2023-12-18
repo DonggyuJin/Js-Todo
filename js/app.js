1e4f40c59d5681e3238013a651509369fde20535
DonggyuJin <jdk2531@gmail.com>
2023-12-18 20:07:37 +0900

    [JS-Todo] feat: Footer 기능 구현 - 전체선택 및 해제 구현 - 카테고리 별 필터 구현 - 삭제 기능 수정

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 const newTodoInput = document.getElementById("newTodo");
 const todoMainLayout = document.getElementsByClassName("todo__list")[0];
+const todoItemLength = document.getElementById("todo__length");
+const todoFooter = document.getElementsByClassName("footer")[0];
+const todoToggleBtn = document.getElementById("todoToggleAll");
+const todoFilterBtn = document.getElementsByClassName("todo__filter")[0];
 
-const todoList = JSON.parse(window.localStorage.getItem("todo-list") || "[]");
+let todoList = JSON.parse(window.localStorage.getItem("todo-list") || "[]");
 
 function createTodoItem(title, id) {
   let todoItem = document.createElement("li");
@@ -30,6 +34,8 @@ function createTodoItem(title, id) {
       todoTitle.style.color = "#000";
       todoTitle.style.animation = "";
     }
+
+    countItemLeft();
   });
 
   todoItem.addEventListener("mouseover", function () {
@@ -38,12 +44,14 @@ function createTodoItem(title, id) {
   todoItem.addEventListener("mouseout", function () {
     todoDestoryBtn.innerHTML = "";
   });
-  todoItem.addEventListener("click", function (e) {
+  todoDestoryBtn.addEventListener("click", function (e) {
     const filteredTodoList = todoList.filter(
       (todo) => todo.id.toString() !== e.target.parentNode.dataset.id
     );
 
-    window.localStorage.setItem("todo-list", JSON.stringify(filteredTodoList));
+    todoList = [...filteredTodoList];
+    window.localStorage.setItem("todo-list", JSON.stringify(todoList));
+    todoItemLength.textContent = todoList.length;
 
     const todoMainLayoutItem = todoMainLayout.querySelectorAll("li");
     todoMainLayoutItem.forEach((li) => {
@@ -51,6 +59,8 @@ function createTodoItem(title, id) {
         li.remove();
       }
     });
+
+    hideFooter();
   });
 
   todoItemBox.appendChild(todoCheckBox);
@@ -61,15 +71,33 @@ function createTodoItem(title, id) {
   todoItem.classList.add("todo__item");
   todoItem.dataset.id = id;
   todoMainLayout.appendChild(todoItem);
+
+  hideFooter();
 }
 
 function initReadTodoItem() {
+  hideFooter();
   todoList.forEach((todo) => {
     createTodoItem(todo.title, todo.id);
   });
 }
 
 initReadTodoItem();
+
+function hideFooter() {
+  if (todoList.length === 0) {
+    todoFooter.style.display = "none";
+  } else {
+    todoFooter.style.display = "flex";
+    countItemLeft();
+  }
+}
+
+function countItemLeft() {
+  todoItemLength.textContent = todoList.filter(
+    (todo) => todo.completed === false
+  ).length;
+}
 
 newTodoInput.addEventListener("keyup", function (e) {
   if (e.key === "Enter") {
@@ -87,6 +115,74 @@ newTodoInput.addEventListener("keyup", function (e) {
     createTodoItem(e.target.value, nowDateTime);
 
     e.target.value = "";
-    todoItem = {};
   }
 });
+
+todoToggleBtn.addEventListener("click", function (e) {
+  todoList.forEach((todo) => {
+    todo.completed = !todo.completed;
+  });
+  window.localStorage.setItem("todo-list", JSON.stringify(todoList));
+
+  const todoMainLayoutItem = todoMainLayout.querySelectorAll("li");
+
+  todoMainLayoutItem.forEach((item) => {
+    let itemCheckBtn = item.children[0].children[0];
+    let itemTitle = item.children[0].children[1];
+    itemCheckBtn.checked = !itemCheckBtn.checked;
+    if (itemCheckBtn.checked === true) {
+      e.target.labels[0].style.color = "#4d4d4d";
+
+      itemTitle.style.textDecoration = "line-through";
+      itemTitle.style.color = "#cccccc";
+      itemTitle.style.animation = "fadeIn 1s ease";
+    } else {
+      e.target.labels[0].style.color = "#e6e6e6";
+
+      itemTitle.style.textDecoration = "";
+      itemTitle.style.color = "#000";
+      itemTitle.style.animation = "";
+    }
+  });
+
+  countItemLeft();
+});
+
+todoFilterBtn.addEventListener("click", function (e) {
+  console.log(e);
+
+  if (e.target.tagName === "A") {
+    e.target.parentNode.parentNode.childNodes.forEach((child) => {
+      child.className = "";
+    });
+    e.target.parentNode.classList.add("todo__filter__selected");
+
+    switch (e.target.textContent) {
+      case "Active":
+        console.log("active");
+        filterTodoList(true);
+        break;
+      case "Completed":
+        filterTodoList(false);
+        break;
+      default:
+        const todoMainLayoutItem = todoMainLayout.querySelectorAll("li");
+        todoMainLayoutItem.forEach((item) => (item.style.display = "flex"));
+    }
+  }
+});
+
+function filterTodoList(completed) {
+  const todoMainLayoutItem = todoMainLayout.querySelectorAll("li");
+  todoList
+    .filter((todo) => todo.completed === completed)
+    .forEach((todo) => {
+      todoMainLayoutItem.forEach((item) => {
+        if (item.dataset.id === todo.id.toString()) {
+          item.style.display = "none";
+        } else {
+          item.style.display = "flex";
+        }
+      });
+    });
+}
